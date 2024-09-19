4de5250291d07c3ae659c3dd90e163991f0d46d5
Chunia <anastasia29082004@yandex.ru>
2024-09-20 02:36:08 +0700

    добавление клиента, заказ, состав заказа

---
+++ sosiska/Model/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sosiska.Model
+{
+    internal class Order
+    {
+        public int Id { get; set; } 
+        public int ClientId { get; set; }
+        public Client Client { get; set; }
+        public Dish Dish { get; set; }  // ??
+        public Menu Menu { get; set; } //??
+    }
+}
---
+++ sosiska/Model/OrderComponent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sosiska.Model
+{
+    internal class OrderComponent
+    {
+        public int OrderCompoID { get; set; }
+        public Dish Dish { get; set; }
+    }
+}
